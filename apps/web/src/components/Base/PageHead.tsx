b721d0a24a5553a1f4d1e0ebf8dfd16a66c77849
andmohiko <andmohiko@gmail.com>
2025-11-19 21:51:25 +0900

    fix: head

--- apps/web/src/components/Base/PageHead.tsx
+++ apps/web/src/components/Base/PageHead.tsx
@@ -2,8 +2,8 @@ import Head from 'next/head'
 
 export const PageHead = (): React.ReactElement => (
   <Head>
-    <title>Firebase Monorepo</title>
-    <meta name="description" content="Firebase Monorepo" />
+    <title>スマレポ</title>
+    <meta name="description" content="スマブラ戦績記録・分析アプリ" />
     <link rel="icon" href="/favicon.ico" />
   </Head>
 )
