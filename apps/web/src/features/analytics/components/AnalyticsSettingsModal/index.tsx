6775e9d4adbd0969eca3a9d05dbd8355a36ce39c
andmohiko <andmohiko@gmail.com>
2026-01-19 17:40:44 +0900

    feat: add analytics settings half modal

---
+++ apps/web/src/features/analytics/components/AnalyticsSettingsModal/index.tsx
@@ -0,0 +1,38 @@
+import { FlexBox } from '~/components/Base/FlexBox'
+import { SwitchInput } from '~/components/Inputs/SwitchInput'
+import { HalfBottomSheet } from '~/components/Modals/HalfBottomSheet'
+import { LabelText } from '~/components/Typography/LabelText'
+import { useAnalyticsSettingsContext } from '~/providers/AnalyticsSettingsProvider'
+
+type Props = {
+  isOpen: boolean
+  onClose: () => void
+}
+
+export const AnalyticsSettingsModal = ({ isOpen, onClose }: Props) => {
+  const { analyticsSettings, setAnalyticsSettings } =
+    useAnalyticsSettingsContext()
+
+  return (
+    <HalfBottomSheet
+      title="マッチアップ分析設定"
+      isOpen={isOpen}
+      onClose={onClose}
+    >
+      <FlexBox direction="row" justify="space-between">
+        <LabelText weight="bold">
+          ダッシュファイターをまとめて表示する
+          {`${analyticsSettings.isGroupDashFighters}`}
+        </LabelText>
+        <SwitchInput
+          value={analyticsSettings.isGroupDashFighters}
+          onChange={(value) => {
+            setAnalyticsSettings({
+              isGroupDashFighters: value,
+            })
+          }}
+        />
+      </FlexBox>
+    </HalfBottomSheet>
+  )
+}
